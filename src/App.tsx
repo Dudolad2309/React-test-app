b65d560e9e7b41058570b3ffc97c8c4baa6d5bb1
Dudolad2309 <77323788+Dudolad2309@users.noreply.github.com>
2021-09-27 23:12:03 +0300

    created a function that changes the array

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
 import React from 'react';
 
+const array: (string | number)[] = [12, 'd', 5, 'h', 'w', 'w', 1, 'a', 'b', 2];
+
+const filterArray = (list: (string | number)[]) => {
+    for (let i = 0; i <= list.length; i++) {
+        if (typeof list[i] === "string") {
+            list.splice(i, 1);
+            i--
+        }
+    }
+    return list
+}
+
+filterArray(array)
+console.log(array);
 
 
 function App() {
-  return (
-    <>
-     Hello
-    </>
-  );
+    return (
+        <>
+            Hello
+        </>
+    );
 }
 
 export default App;
